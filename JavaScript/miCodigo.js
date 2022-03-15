7ed6b2d580df716f0859ad2b37d9330b183fbbe6
NathaliaRodriguez <nathalia_c.rodriguez@uao.edu.co>
2022-03-14 19:59:41 -0500

    JavaScript
    Introducción al Javascript, ejercicios en clase

---
+++ JavaScript/miCodigo.js
@@ -0,0 +1,22 @@
+/* console.log("Hola Mundo");
+let rol;
+console.log(rol);
+rol ="student";
+console.log(rol);
+console.log("Hello I'm "+rol+" From UAO");
+console.log(`Hola soy ${rol} de la UAO`);
+let cadena = "1";
+let numero = 1;
+if(cadena== numero && cadena === numero){
+    console.log("Iguales");
+} else {
+    console.log("No Iguales")
+} */
+
+let a=35;
+let b=20;
+
+let res = a / b;
+
+console.log(res);
+console.log(Math.round(res));
